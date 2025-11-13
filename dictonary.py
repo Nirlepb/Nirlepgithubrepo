caf96cbfed32eb352c9dc62c1493ed371a4cae0f
Boddupally Nirlep <228563259+Nirlepb@users.noreply.github.com>
2025-11-13 22:31:34 +0530

    practice

---
+++ dictonary.py
@@ -0,0 +1,8 @@
+n=int(input("enter no of items:"))
+my_dic={}
+for c in range(n):
+    key=input("enter key:")
+    value=input("entervalue:")
+    my_dic[key]=value
+print("dictonary",my_dic)    
+    
---
+++ sets.py
@@ -0,0 +1,14 @@
+# set_a={1,2,3,4,5}
+# set_a.add(10)
+# set_a.discard(3)
+
+# nipp=[1,2,2,3,4,4,5]
+# lis=set(nipp)
+# print(lis)
+# print(set_a)
+# a=set()
+# user=input("enter a word:").strip()
+# a.update(user)
+# f=len((a))
+# print(f)
+
