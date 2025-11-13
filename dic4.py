2aa57af9dc86043a53d141268c1afce5ebc921c9
Boddupally Nirlep <228563259+Nirlepb@users.noreply.github.com>
2025-11-13 23:09:37 +0530

    done

--- dic4.py
+++ dic4.py
@@ -1,5 +1,13 @@
-original = {'a': 1, 'b': 2, 'c': 3}
-
-swapped = {value: key for key, value in original.items()}
-
-print(swapped)
+def nir(a):#i am difing function
+    ni={}# an emty dic
+    for i in a:#for i in some value of a 
+        if i in ni:# and if i also in dictonary
+            ni[i]+=1#then add the number
+        else:
+            ni[i]=1#else giving number
+    print(ni)         
+n=[1001, 1002, 1003, 1002, 1004, 1005, 1001, 1003, 1002]
+nir(n)
+            
+    
+                
